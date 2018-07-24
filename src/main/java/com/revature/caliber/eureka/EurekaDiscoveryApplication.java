28417b2255de9de572158ddc38646588fcbaa588
Patrick Walsh <patrick.walsh@revature.com>
2018-07-24 14:40:38 -0400

    evaluation service

--- src/main/java/com/revature/caliber/eureka/EurekaDiscoveryApplication.java
+++ src/main/java/com/revature/caliber/eureka/EurekaDiscoveryApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class Application {
+public class EurekaDiscoveryApplication {
 	
 	public static void main(String[] args) throws Exception {
-		SpringApplication.run(Application.class, args);
+		SpringApplication.run(EurekaDiscoveryApplication.class, args);
 	}
 
 }
